3f637ddef4d7dd1c690b42da2f8ea2e637651acc
mohit10696 <41354652+mohit10696@users.noreply.github.com>
2019-07-02 10:19:39 +0530

    Update fibo_it.java

--- fibo_it.java
+++ fibo_it.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 public class fibo_it {
   public static void main(String[] args) {
-   int first = 0 ; 
-   int second = 1 ; 
-   int output=0;
-   int count= 0; 
+   int count = 0;
+   int first = 0 ;        count++;
+   int second = 1 ;       count++;
+   int output=0;          count++;
    Scanner sc = new Scanner(System.in);
    int length = sc.nextInt();
-   if(length==1)output=1;
-   for(int i = 2 ; i <= length ; i++ )
+   count++;
+   if(length==1){output=1; count++;}
+   count+=2;
+   for(int i = 2 ; i <= length ; i++ ,count+=2 );
    {
-       output = first + second;
-       first = second;
-       second = output;
+       output = first + second;   count++;
+       first = second;             count++;
+       second = output;             count++;
    }
    System.out.println(output);  
+      System.out.println(count);
 }  
 }
