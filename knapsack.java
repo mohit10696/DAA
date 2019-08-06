c3f2ab155acb0864697188229fa78279a87b7712
Mohit Sojitra <41354652+mohit10696@users.noreply.github.com>
2019-08-06 09:58:11 +0530

    Update knapsack.java

--- knapsack.java
+++ knapsack.java
@@ -4,6 +4,7 @@ class product{
     int weight;
     int value;
     float ratio ;
+    int x;
     public void assratio()
     {
         ratio = (float)this.value/this.weight;
@@ -13,7 +14,8 @@ public class knapsack {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Size of product");
-        product x[] = new product[sc.nextInt()];
+        int l = sc.nextInt();
+        product x[] = new product[l];
         System.out.println("Enter weight & value");
         for(int i = 0 ; i < x.length ; i++)
         {
@@ -23,15 +25,29 @@ public class knapsack {
             x[i].assratio();
             System.out.println("");
         }
-        for (int i = 0; i < n-1; i++)
-		for (int j = 0; j < n-i-1; j++)
-			if (arr[j] > arr[j+1])
+        System.out.println("Enter required rate");
+        int W = sc.nextInt();
+        for (int i = 0; i < l-1; i++)
+		for (int j = 0; j < l-i-1; j++)
+			if (x[j].ratio > x[j+1].ratio)
 			{
 					// swap temp and arr[i]
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+                                        product temp = new product();
+					temp = x[j];
+					x[j] = x[j+1];
+					x[j+1] = temp;
 			}
-        
+        int i = 0,weight=0;
+        while(i<l && weight < W)
+        {
+            if(weight + x[i].weight <= W)x[i].x = 1 ;
+            else x[i].x = (W - weight)/x[i].weight;
+            weight = weight + x[i].x*x[i].weight;
+            i++;
+        }
+        for(product n : x)
+        {
+            System.out.println("Product "+n.weight+"ratio"+n.x);
+        }
     }
 }
